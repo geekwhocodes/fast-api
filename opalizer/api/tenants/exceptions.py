d6280ba0d3a273b055affff59035486e8ba5f5e7
Ganesh R <geekwhocodes@outlook.com>
2023-07-06 00:57:13 +0530

    Revert "init"
    This reverts commit fe96e611f97094fdcbc4ff3b3a0d5b264c7770a1.

--- opalizer/api/tenants/exceptions.py
+++
@@ -1,4 +0,0 @@
-class TenantNotFoundError(Exception):
-    def __init__(self, tenant_id):
-        self.message = f"Tenant {tenant_id} not found!"
-        super().__init__(self.message)
--- opalizer/core/logging.py
+++
@@ -1,3 +0,0 @@
-def setup_logging():
-    pass
-    # logging.basicConfig(filename=os.path.join(root_dir, "logging.ini"))
